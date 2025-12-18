7852844b11e3ec0ea770fd2f4403928043309e2c
Vausly <172235138+Vausly@users.noreply.github.com>
2025-12-18 16:38:08 +0700

    fix paragraph

--- src/pages/story.tsx
+++ src/pages/story.tsx
@@ -11,19 +11,19 @@ const Story = () => {
                     <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center text-purple-300">
                         Short Story
                     </h1>
-                    My Journey & The Story Behind “Vausly”
+                    <p>My Journey & The Story Behind “Vausly”</p>
 
-                    My journey in the digital world began in 2013 when internet access first arrived at my home. At first, I only used it for entertainment — playing games and watching videos — but everything changed in 2015 when I started writing short stories in notebook apps on my phone. That was the moment I realized I enjoyed creating, not just consuming.
+                    <p>My journey in the digital world began in 2013 when internet access first arrived at my home. At first, I only used it for entertainment — playing games and watching videos — but everything changed in 2015 when I started writing short stories in notebook apps on my phone. That was the moment I realized I enjoyed creating, not just consuming.</p>
 
-                    In 2017, I launched my first <a href="https://www.youtube.com/channel/UCabb5RfYa62j44O_uUPZI5A?sub_confirmation=1">YouTube channel just for watching</a>, then create the <a href="https://www.youtube.com/channel/UCC8XmP-H3_bUKavcudu_M9g?sub_confirmation=1">another one</a> and uploaded my first video on 31 December 2018. After many challenges and failures, I finally reached my first 1,000 subscribers in April 2023, a small milestone that meant a lot to me.
+                    <p>In 2017, I launched my first <a href="https://www.youtube.com/channel/UCabb5RfYa62j44O_uUPZI5A?sub_confirmation=1">YouTube channel just for watching</a>, then create the <a href="https://www.youtube.com/channel/UCC8XmP-H3_bUKavcudu_M9g?sub_confirmation=1">another one</a> and uploaded my first video on 31 December 2018. After many challenges and failures, I finally reached my first 1,000 subscribers in April 2023, a small milestone that meant a lot to me</p>.
                     
-                    In 2018, I published my early fictional stories on my personal website and began shaping my identity as a digital creator. Then, 6 years later, I decided to change my brand name from Lesunk Media to Vausly Media.
+                    <p>In 2018, I published my early fictional stories on my personal website and began shaping my identity as a digital creator. Then, 6 years later, I decided to change my brand name from Lesunk Media to Vausly Media.</p>
 
-                    The name “Vausly” came from a combination of two simple ideas. “Vaus” was inspired by a phishing virus and website system called <a href="https://web.archive.org/web/20181218022622/vaussneim.net">Vaussneim.net</a>, a name that stayed in my memory for years. The suffix “-ly” was chosen because it is commonly used in modern tech brands like Bitly, Linkly, and similar platforms that making the name feel short, clean, and easy to remember. My main goal was to build a brand that feels modern, memorable, and lightweight.
+                    <p>The name “Vausly” came from a combination of two simple ideas. “Vaus” was inspired by a phishing virus and website system called <a href="https://web.archive.org/web/20181218022622/vaussneim.net">Vaussneim.net</a>, a name that stayed in my memory for years. The suffix “-ly” was chosen because it is commonly used in modern tech brands like Bitly, Linkly, and similar platforms that making the name feel short, clean, and easy to remember. My main goal was to build a brand that feels modern, memorable, and lightweight.</p>
 
-                    From 2016 to 2019, I became very active in creative projects, especially in the Minecraft community especially UTK.io by Maple Media. I created maps, mods, and other in-game content, and shared them through various community platforms. I was also involved with a creator group that helped me grow faster and understand how digital communities work.
+                    <p>From 2016 to 2019, I became very active in creative projects, especially in the Minecraft community especially UTK.io by Maple Media. I created maps, mods, and other in-game content, and shared them through various community platforms. I was also involved with a creator group that helped me grow faster and understand how digital communities work.</p>
 
-                    Today, Vausly Media represents my journey as a creator, developer, and storyteller. Everything I build like websites, games, stories, and tools that all come from that simple passion I discovered back in 2015: the desire to create something meaningful and memorable.
+                    <p>Today, Vausly Media represents my journey as a creator, developer, and storyteller. Everything I build like websites, games, stories, and tools that all come from that simple passion I discovered back in 2015: the desire to create something meaningful and memorable.</p>
                                         
                 </div>
             </PageWrapper>
